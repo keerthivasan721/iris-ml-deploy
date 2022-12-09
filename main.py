9dc538083ca140d0d1e3ed7e4b6e8d6a817065f2
keerthi vasan <keerthivasan676@gmail.com>
2022-12-09 17:51:15 +0530

    removed unwanted codes

--- main.py
+++ main.py
@@ -14,8 +14,3 @@ model.fit(x,y)
 
 with open('rf.pkl','wb') as f:
     pickle.dump(model,f)
-
-# with open('rf.pkl','rb') as f:
-#     model = pickle.load(f)
-
-# print(model.predict(np.array([2.4,2.5,0.5,0.5]).reshape(1,-1)))
